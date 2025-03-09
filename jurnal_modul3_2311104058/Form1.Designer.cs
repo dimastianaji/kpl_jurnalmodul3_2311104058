ae368df1c4d831f234c15073ae1c557c5366558d
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-03-09 15:18:31 +0700

    menambahkan layout GUI kalkulator

--- jurnal_modul3_2311104058/Form1.Designer.cs
+++ jurnal_modul3_2311104058/Form1.Designer.cs
@@ -28,13 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textTotal = new System.Windows.Forms.TextBox();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.samaDenganBtn = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textTotal
+            // 
+            this.textTotal.Location = new System.Drawing.Point(12, 12);
+            this.textTotal.Multiline = true;
+            this.textTotal.Name = "textTotal";
+            this.textTotal.Size = new System.Drawing.Size(776, 98);
+            this.textTotal.TabIndex = 0;
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Location = new System.Drawing.Point(12, 127);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(247, 69);
+            this.plusBtn.TabIndex = 1;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            // 
+            // samaDenganBtn
+            // 
+            this.samaDenganBtn.Location = new System.Drawing.Point(265, 127);
+            this.samaDenganBtn.Name = "samaDenganBtn";
+            this.samaDenganBtn.Size = new System.Drawing.Size(266, 69);
+            this.samaDenganBtn.TabIndex = 2;
+            this.samaDenganBtn.Text = "=";
+            this.samaDenganBtn.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(537, 127);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(251, 69);
+            this.button0.TabIndex = 3;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 69);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(266, 69);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(537, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 69);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 277);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(247, 69);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(265, 277);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(265, 69);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(537, 277);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(251, 69);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 352);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(247, 69);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(265, 352);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(265, 69);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(537, 352);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(251, 69);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.samaDenganBtn);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.textTotal);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textTotal;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.Button samaDenganBtn;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- jurnal_modul3_2311104058/Form1.cs
+++ jurnal_modul3_2311104058/Form1.cs
@@ -16,5 +16,55 @@ namespace jurnal_modul3_2311104058
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button9.Text;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textTotal.Text = textTotal.Text + button0.Text;
+        }
     }
 }
